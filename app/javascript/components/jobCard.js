2263c26413656aeae5ae40196e403c25b482508f
Timothy Browne <timbrowne066@gmail.com>
2018-01-17 13:55:47 -0500

    show page arrangement and ridiculous date/time parsing

--- app/javascript/components/jobCard.js
+++ app/javascript/components/jobCard.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const JobCard = (props) => {
+  let date = props.time
+  let date2 = new Date(date).toLocaleDateString("en-us")
   return (
     <div className="JobCard">
       <h2>{props.title}</h2>
       <p>{props.summary}</p>
+      <p>{date2}</p>
     </div>
   )
 }
--- app/javascript/containers/jobShowContainer.js
+++ app/javascript/containers/jobShowContainer.js
@@ -9,10 +9,20 @@ class JobShowContainer extends React.Component {
   }
 
   render() {
+    let date = this.props.job.created_at
+    let date2 = new Date(date).toLocaleDateString("en-us")
+    console.log(date2)
 
     return (
       <div className="JobShowContainer">
         <h1>Job Show Container</h1>
+        <h3>{this.props.job.title}</h3>
+        <p>{this.props.job.city}, {this.props.job.state}<br/>{this.props.job.zipcode}</p>
+        <p>{this.props.job.summary}</p>
+        <p>{this.props.job.description}</p>
+        <p>{this.props.job.requirements}</p>
+        <p>{date2}</p>
+
       </div>
     );
   }
