549843d2c3142eaabbf682e652a335b210de33d3
Timothy Browne <timbrowne066@gmail.com>
2018-01-23 16:07:13 -0500

    styling home page

--- app/javascript/components/navBar.js
+++ app/javascript/components/navBar.js
@@ -18,11 +18,10 @@ class NavBar extends React.Component {
           <span></span>
           <span></span>
           <ul id="menu">
-            <a href="#"><li>Home</li></a>
-            <a href="#"><li>About</li></a>
-            <a href="#"><li>Info</li></a>
+            <a href="/"><li>Home</li></a>
+            <a href="#"><li>About Us</li></a>
+            <a href="#"><li>Job Listings</li></a>
             <a href="#"><li>Contact</li></a>
-            <a href="https://erikterwan.com/" target="_blank"><li>Show me more</li></a>
           </ul>
         </div>
       </nav>
--- app/javascript/containers/homeContainer.js
+++ app/javascript/containers/homeContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Carousel, Image, Navbar, NavItem, MenuItem, NavDropdown, Nav, PanelGroup, Panel, Grid, Row, Col, Button, Well } from 'react-bootstrap';
+import Logo from '../../assets/images/logo.png';
 
 class HomeContainer extends React.Component {
   constructor(props) {
@@ -14,13 +15,15 @@ class HomeContainer extends React.Component {
     return (
       <div className="HomeContainer">
         <section id="home-1">
-          <Grid>
-            <Row className="welcome">
-              <Col className="text-center" sm={8} smOffset={2}>
-                <h1 className="text-center">Hello</h1>
-              </Col>
-            </Row>
-          </Grid>
+
+              <Image className="homeLogo" src={Logo}/>
+              <div className="welcome">
+                <h1>The IT recruiting model is broken.</h1>
+                <h2>So we fixed it.</h2>
+              </div>
+          <div className="arrow">
+            <a href="home-2"><i className="fa fa-chevron-down"></i></a>
+          </div>
         </section>
         <section id="home-2"></section>
         <section id="home-3"></section>
