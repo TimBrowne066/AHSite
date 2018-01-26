17d67d5f53f4e14751f4eda735b933edd16bc83e
Timothy Browne <timbrowne066@gmail.com>
2018-01-26 12:29:31 -0500

    contact module

--- app/javascript/components/partnerCarousel.js
+++ app/javascript/components/partnerCarousel.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import { Carousel, Image, Navbar, NavItem, MenuItem, NavDropdown, Nav, PanelGroup, Panel, Grid, Row, Col, Button, Well } from 'react-bootstrap';
+import Car1 from '../../assets/images/postcard_1_linkedin_picture.png';
+import Car2 from '../../assets/images/postcard_1_valentines_sample_small.png';
 
 const PartnerCarousel = (props) => {
   return (
     <div className="PartnerCarousel">
-      <Carousel>
+      <Carousel prevIcon='' nextIcon='' indicators={false}>
         <Carousel.Item>
-          <Image className="carousel-image" src={}/>
-          <Carousel.Caption>
-            <h3>Hello</h3>
-          </Carousel.Caption>
+          <Image className="carousel-image" src={Car1}/>
         </Carousel.Item>
         <Carousel.Item>
-          <Image className="carousel-image" src={}/>
-          <Carousel.Caption>
-            <h3>Hello</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <Image className="carousel-image" src={}/>
-          <Carousel.Caption>
-            <h3>Hello</h3>
-          </Carousel.Caption>
+          <Image className="carousel-image" src={Car2}/>
         </Carousel.Item>
       </Carousel>
     </div>
--- app/javascript/containers/homeContainer.js
+++ app/javascript/containers/homeContainer.js
@@ -3,6 +3,11 @@ import { Carousel, Image, Navbar, NavItem, MenuItem, NavDropdown, Nav, PanelGrou
 import Logo from '../../assets/images/logo1.png';
 import Me from '../../assets/images/me_panel.png';
 import { Fade, Flip, Rotate, Zoom } from 'react-reveal';
+import PartnerCarousel from '../components/partnerCarousel'
+import Twitter from '../../assets/images/twitter.png';
+import Tumblr from '../../assets/images/tumblr.png';
+import Email from '../../assets/images/email.png';
+import Phone from '../../assets/images/phone.png';
 
 class HomeContainer extends React.Component {
   constructor(props) {
@@ -18,13 +23,19 @@ class HomeContainer extends React.Component {
       <div className="HomeContainer">
         <div className="section" id="home-1">
           <Row>
-            <Col sm={8} smOffset={2}>
+            <Col>
               <Image className="homeLogo" src={Logo}/>
             </Col>
           </Row>
               <div className="welcome">
-                <h1 className="typewriter">The IT recruiting model is broken.</h1>
+                <h1>The IT recruiting model is broken.</h1>
                 <Zoom delay={500}><h2>So we fixed it.</h2></Zoom>
+                <div className="icon-group">
+                  <a href="https://twitter.com/agilehires?lang=en"><Fade delay={250}><img className="icon" src={Twitter}/></Fade></a>
+                  <a href="http://agilehires.tumblr.com/"><Fade delay={500}><img className="icon" src={Tumblr}/></Fade></a>
+                  <a href="mailto:arie@agilehires.com"><Fade delay={750}><img className="icon" src={Email}/></Fade></a>
+                  <a href="tel:215-277-5680"><Fade delay={1000}><img className="icon" src={Phone}/></Fade></a>
+                </div>
               </div>
           <div className="arrow">
             <a href="#home-2"><i className="fa fa-chevron-down"></i></a>
@@ -58,7 +69,9 @@ class HomeContainer extends React.Component {
               </Row>
           </div></Fade>
         </div>
-        <div className="section" id="home-3"></div>
+        <div className="section" id="home-3">
+          <PartnerCarousel/>
+        </div>
         <div className="section" id="home-4"></div>
       </div>
     );
